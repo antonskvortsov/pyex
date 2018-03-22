1160c79908e6ae2c43e91be3725abdb66680b947
toxx <skvo@toxx.me>
2018-03-22 16:14:15 +0300

    First Commit

--- basic_examples/python_os_module.py
+++ basic_examples/python_os_module.py
@@ -6,7 +6,7 @@
 
 import os
 
-print 'Your operating system: ', os.name
-print 'Your username: ', os.getlogin()
-print 'Your current working directory is: ', os.getcwd()
-print 'Your system path is: ', os.getenv('PATH')
+print ('Your operating system: ', os.name)
+print ('Your username: ', os.getlogin())
+print ('Your current working directory is: ', os.getcwd())
+print ('Your system path is: ', os.getenv('PATH'))
